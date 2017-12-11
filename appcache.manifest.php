f89c3dbd8c4888acd4aa873b7c887a9eb46dc0cc
100naaa0 <34256587+100naaa0@users.noreply.github.com>
2017-12-12 08:45:58 +0900

    Update appcache.manifest.php

--- appcache.manifest.php
+++ appcache.manifest.php
@@ -2,6 +2,22 @@ ACHE MANIFEST
 # version 0.1.0
 
 CACHE:
+css/background.png
+css/level1_all.png
+css/level2_sprites_w120.png
+css/level3_source_1.gif
+css/level3_source_2.gif
+css/level3_source_3.gif
+css/level3_source_4.gif
+css/level3_source_5.gif
+css/level3_source_6.gif
+css/level3_source_7.gif
+css/level3_source_8.gif
+css/level3_source_9.gif
+css/level3_source_10.gif
+css/level3_source_11.gif
+css/level3_source_12.gif
+css/level3_source_13.gif
 css/style.css
 js/index.js
 http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
